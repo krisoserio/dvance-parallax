85afea459b8ee3f436e290c4fecc0d6a2b1a72a4
krisoserio <khristianoserio50@gmail.com>
2021-01-07 16:39:18 +0800

    parallax

---
+++ assets/js/advanceparallax.js
@@ -0,0 +1,14 @@
+const translate = document.querySelectorAll(".translate");
+
+window.addEventListener('scroll', () => {
+
+    let scroll = window.pageYOffset;
+    // console.log(scroll);
+
+
+    translate.forEach(element => {
+        let speed = element.dataset.speed;
+        // console.log(speed);
+        element.style.transform = `translateY(${scroll * speed}px)`;
+    })
+})
